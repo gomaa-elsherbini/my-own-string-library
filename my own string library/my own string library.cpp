0413038f7c7291ab2d264414a520c4e249eb58aa
gomaa <gogochemist35@gmail.com>
2023-12-05 19:45:17 +0200

    Add project files.

---
+++ my own string library/my own string library.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "clsString.h";
+using namespace std;
+
+
+
+
+int main()
+{
+	clsString string1;
+	clsString string2("ali Mahmoud");
+
+
+
+
+
+
+
+
+
+
+	system("pause>0");
+	return 0;
+}
